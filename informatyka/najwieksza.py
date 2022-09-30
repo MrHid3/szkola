a6db18d6f43386373826dfbfaadd9f7b388533f2
Gabriel Kornaś <Korna Gabriel@ppinf.local>
2022-09-30 13:24:27 +0200

    informatyka

--- informatyka/najwieksza.py
+++ informatyka/najwieksza.py
@@ -3,9 +3,11 @@ c = int(input())
 b = []
 x = 0
 for i in range(c):
+    print("Liczba ",i+1,":", end=' ')
     a = int(input())
     b.append(a)
+    print( )
 for i in range(len(b)):
     if b[i] > x:
         x = b[i]
-print("Najwieksza jest ", x)
+print("Najwieksza jest", x)
