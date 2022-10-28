5c6113651a4ffaf3aad0adbf6adece83e32344ce
Gabriel Kornaś <Korna Gabriel@PPINF.LOCAL>
2022-10-28 13:52:39 +0200

    informatyka

--- informatyka/rok_1/semestr_1/październik/pesel/run.py
+++ informatyka/rok_1/semestr_1/październik/pesel/run.py
@@ -30,3 +30,23 @@ rok = (stulecie*100)+int(pesel[0] + pesel[1])
 
 #Dzień urodzenia
 dzien = int(pesel[4]+pesel[5])
+
+#Plec
+if int(pesel[9])%2 == 1:
+    plec = 1
+else:
+    plec = 0
+
+#Suma kontrolna
+suma_kon = 10 - ((int(pesel[0]) + int(pesel[1])*3 + int(pesel[2])*7 + int(pesel[3])*9 + int(pesel[4]) + int(pesel[5])*3 + int(pesel[6])*7 + int(pesel[7])*9 + int(pesel[8])*1 + int(pesel[9])*3) % 10) % 10
+
+if pesel[10] != suma_kon:
+    print("nieprawidlowy pesel")
+
+#Podsumowanie
+else:
+    print(f"Ta osoba urodziła się {dzien} dnia {miesiac} miesiąca {rok} roku")
+    if plec == 1:
+        print("Jest to mężczyzna")
+    else:
+        print("Jest to kobieta")
