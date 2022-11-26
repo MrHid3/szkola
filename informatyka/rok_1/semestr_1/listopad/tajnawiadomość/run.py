e4249d8c8634bf582bcac0ca1ede7f4915ffc87d
bozgi <47087484+bozgi@users.noreply.github.com>
2022-11-26 21:43:41 +0100

    Działające rozszyfrowanie
    Prawidłowo wykonuje xor oraz posiada działające dekodowanie wartości h. 
    
    wynik = b'veni vidi vici'

--- informatyka/rok_1/semestr_1/listopad/tajnawiadomość/run.py
+++ informatyka/rok_1/semestr_1/listopad/tajnawiadomość/run.py
@@ -1,2 +1,17 @@
 import base64
-print(hex(int("584b51f48bca9572acd08d378362", base=16) ^ int("2e2e3f9dabbcfc16c5f0fb5ee00b", base=16)).upper())
+
+key = 0x584b51f48bca9572acd08d378362
+text = 0x2e2e3f9dabbcfc16c5f0fb5ee00b
+
+xor = key ^ text
+
+h = str(hex(xor)).upper()
+
+print(h)
+
+wynik = h[2:]
+
+print(base64.b16decode(wynik))
+
+# Pozdrowienia z Bali
+# bozgi
