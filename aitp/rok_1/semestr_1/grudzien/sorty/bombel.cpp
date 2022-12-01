5930657ee361d1fe745ab79145062977881c1d8e
Laptop dom <gkornas12@gmail.com>
2022-12-01 07:06:00 +0000

    bubblesort

---
+++ aitp/rok_1/semestr_1/grudzien/sorty/bombel.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+
+vodi zamien(int &a, int &b){
+    int pom = a;
+    b = pom;
+}
+
+void bubbleSort(int tab[],int n){
+    bool zamian;'
+    for(int j=1;j<n;j++){
+        zamiana=false;
+        for(int i=0;i<n-j;i++){
+            zamien(tab[i],tab[i+1]);
+            zamiana=true;
+        }
+    }
+    if (!zamian) return;
+}
+int main(){
+
+}
