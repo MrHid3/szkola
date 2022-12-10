5a8b9cf87af9e105c2071824775bcee54126f66a
Laptop dom <gkornas12@gmail.com>
2022-12-10 10:56:53 +0000

    update

---
+++ informatyka/rok_1/semestr_1/dec/makedir/run.py
@@ -0,0 +1,7 @@
+import os
+
+os.makedirs('1p/grupa1/jkowalski')
+os.makedirs('1p/grupa1/okowalska')
+os.makedirs('1p/grupa1/anowak')
+os.makedirs('1p/grupa2/ekowalska')
+os.makedirs('1p/grupa2/jnowak')
