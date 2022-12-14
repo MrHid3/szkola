ecaa0e1b2afcea9c5aba5d16a3a9093b4a1ad1dc
bozgi <47087484+bozgi@users.noreply.github.com>
2022-12-14 20:10:28 +0100

    działa jak natura chciała
    Gość od informatyki w kulki poleciał i wysłał nam link, który nie mówi jak używać makedirs()
    
    Pozdrowienia z Bali!

--- informatyka/rok_1/semestr_1/dec/makedir/run.py
+++ informatyka/rok_1/semestr_1/dec/makedir/run.py
@@ -1,7 +1,15 @@
 import os
 
-os.makedirs('1p/grupa1/jkowalski')
-os.makedirs('1p/grupa1/okowalska')
-os.makedirs('1p/grupa1/anowak')
-os.makedirs('1p/grupa2/ekowalska')
-os.makedirs('1p/grupa2/jnowak')
+f = open("data.txt")
+
+for x in f:
+    path = x.replace('_', '//')
+    path = path.replace('\n', '')
+    os.makedirs(path)
+    
+# dla data.txt
+# 1p_grupa1_jkowalski
+# 1p_grupa1_okowalska
+# 1p_grupa1_anowak
+# 1p_grupa2_ekowalska
+# 1p_grupa2_jnowak
